8f3dfbe2b92b86c18401eb355d4919e6de999454
root <root@localhost.(none)>
2016-01-02 09:50:14 -0500

    v5

--- static/js/zhangyue.js
+++ static/js/zhangyue.js
@@ -2,8 +2,11 @@ $(document).ready(function() {
     $('body').on('click', 'button[id*=v_edit]', function() {
 	    var $this = $(this);
 	    var $host = $($this.parent().parent().find('strong')).text();
+	    var $select_index = $($this.parent().parent().find('select'));
+	    var $select_file = $select_index.find("option:selected").text();
 	    //console.log($host);
 	    alert($host);
+	    alert($select_file);
     });
     $('body').on('click', 'button[id*=v_test]', function() {
 	    var $this = $(this);
