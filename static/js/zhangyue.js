f8f3ad6d8bb977469f531f02ed1032cfbee92aa1
root <root@localhost.(none)>
2016-01-02 07:12:53 -0500

    v2

--- static/js/zhangyue.js
+++ static/js/zhangyue.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-    $('#example').DataTable();
-} );
+    $('body').on('click', 'button[id*=v_edit]', function() {
+            alert("111");
+    });
+    $('body').on('click', 'button[id*=v_test]', function() {
+            alert("2222");
+    });
+    $('body').on('click', 'button[id*=v_reload]', function() {
+            alert("3333");
+    });
+});
